1a3d6202c5a69d1888fbc5b2980cfac51771bfe8
rollingwaves57 <madhurrawat137@gmail.com>
2016-12-01 22:29:00 +0530

    cbgp codes

--- getASprefixes.py
+++ getASprefixes.py
@@ -3,26 +3,39 @@
 import urllib2
 import operator
 from bs4 import BeautifulSoup
+COUNTRY_CODE = 'EG'
 
-AS_LIST_TO_RANK = './AS_NUMBER.txt'
+AS_LIST_TO_RANK = './'+COUNTRY_CODE+'_AS.txt'
 
 with open(AS_LIST_TO_RANK) as f:
 	ASES = f.readlines()
 
-fo=open('./AS_prefixes.txt', 'w')
+out_file = './'+COUNTRY_CODE+'_ASPrefixes.txt'
+
+fo=open(out_file, 'w')
+
+print 'out_file : '+out_file
 
 for ASS in ASES:
 	AS = ASS[0:len(ASS)-1]
 	ascount=0
 	url = 'http://www.cidr-report.org/cgi-bin/as-report?as='+AS+'&view=2.0'
-	print 'Ranking '+AS
+	print
+	print 'Fetch : '+url
 	html_doc = urllib2.urlopen(url)
 	soup = BeautifulSoup(html_doc)
 	uls = soup.find_all('ul')
 	pres = uls[2].find_all('pre')
+	print len(pres)
+	if len(pres)<2:
+		continue
 	links = pres[1].find_all('a')
 	for link in links[1:]:
 		classes = link['class']
 		colorclass = classes[0]
 		if(colorclass=='green' or colorclass=='black'):
-			print link.text()
+			print link.getText()
+			fo.write(AS+' '+link.getText()+'\n')
+			break
+
+
--- mcut.py
+++ mcut.py
@@ -4,15 +4,20 @@ import itertools
 from random import randint
 
 def find_cuts(G):
-	r = nx.node_connectivity(G) #minimum number of nodes required to disconnect the graph.
+	"""
+	minimum number_of_nodes required to disconnect a Graph.
+	"""
+	r = nx.node_connectivity(G) 
 
-	possible_cuts = itertools.combinations(iter(G.nodes()), r) #get all combinations of size r.
-	# print possible_cuts
+	"""
+	get all combinations of size r.
+	"""
+	possible_cuts = itertools.combinations(iter(G.nodes()), r)
+	
 	max_pf=0
 	max_cut=()
 	tie=False
 	for possible_cut in possible_cuts:
-		# print possible_cut
 		H = G.copy()
 		H.remove_nodes_from(possible_cut)
 		if not nx.is_connected(H):
@@ -32,6 +37,9 @@ def find_cuts(G):
 	G.remove_nodes_from(max_cut)
 	remainingGs = list(nx.connected_component_subgraphs(G))
 
+	"""
+	draw Graph
+	"""
 	pos = nx.spring_layout(G)
 	nx.draw_networkx_nodes(G, pos=pos, nodelist = G.nodes())
 	nx.draw_networkx_edges(G, pos=pos, edgelist = G.edges())
@@ -44,18 +52,31 @@ def find_cuts(G):
 
 
 pf_dict={}
+
+"""
+number of nodes we want to add to initial graph.
+"""
 n_nodes=30
 
 G=nx.Graph()
 for node in range(1,n_nodes+1):
 	G.add_node(str(node))
 
+"""
+number of edges to add to our graph.
+"""
+n_edges=100
 
 for i in range(1,100):
+	"""
+	get two random nodes to create an edge.
+	"""
 	n1=randint(1,n_nodes)
 	n2=randint(1,n_nodes)
 	sn1=str(n1)
 	sn2=str(n2)
+
+	"maintain a dictionary to count the path frequency of each node."
 	if sn1 in pf_dict:
 		pf_dict[sn1]=pf_dict[sn1]+1
 	else:
