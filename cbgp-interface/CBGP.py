7ce2b20382d0df411b0f36aa05db8bf7a8f3ce8d
rollingwaves57 <madhurrawat137@gmail.com>
2017-07-10 23:48:00 +0530

    fix cbgp interface

--- cbgp-interface/CBGP.py
+++ cbgp-interface/CBGP.py
@@ -9,6 +9,11 @@
 import os, select, Queue, thread, threading, sys, posix, time;
 import fcntl
 
+import argparse
+
+#local imports
+import constants
+
 class CBGP_reader(threading.Thread):
   rHandle = None
   qOutput = None
@@ -66,20 +71,65 @@ class CBGP:
     self.wHandle.close()
 
 if __name__ == '__main__':
-  # cBGP = CBGP("/home/madhurubuntu/madhur-work/cbgp-2.3.2/src/cbgp")
+
+  parser = argparse.ArgumentParser(description = 'create cli for country to country all to all')
+  parser.add_argument('-c', '--country_code', help='Country Code', required = True)
+  parser.add_argument('-m', '--mode', help='C:Country G2C:Global to Country A2C:Asmatra to Country', required = True)
+  parser.add_argument('-p', '--cli_number', help='cli file number', required = True)
+
+
+  args = parser.parse_args()
+  COUNTRY_CODE = args.country_code
+  CLI_NUMBER = args.cli_number
+  MODE = args.mode
+
+  if MODE == 'C':
+    SUFFIX = "_country_"
+    SUFFIX_TRACEROUTE = '_country_traceroute_'
+  elif MODE == "G2C" or MODE == "g2c":
+    SUFFIX = "_g2c_"
+    SUFFIX_TRACEROUTE = '_g2c_traceroute_'
+  elif MODE == "a2c" or MODE == "A2C":
+    SUFFIX = "_a2c_"
+    SUFFIX_TRACEROUTE = '_a2c_traceroute_'
+  elif MODE == 'T':
+    SUFFIX = "_transport_"
+    SUFFIX_TRACEROUTE = "_transport_traceroute_"
+  elif MODE == "B":
+    SUFFIX = "_bank_"
+    SUFFIX_TRACEROUTE = "_bank_traceroute_"
+  elif MODE == "G":
+    SUFFIX = "_govt_"
+    SUFFIX_TRACEROUTE = "_govt_traceroute_"
+  elif MODE == "D":
+    SUFFIX = "_dns_"
+    SUFFIX_TRACEROUTE = "_dns_traceroute_"
+
+  loading_file = constants.TEST_DATA + COUNTRY_CODE + "/" + COUNTRY_CODE + SUFFIX + CLI_NUMBER + '.cli'
+  trace_file = constants.TEST_DATA + COUNTRY_CODE + "/" + COUNTRY_CODE + SUFFIX_TRACEROUTE + CLI_NUMBER + '.cli'
+
+  out_cbgp_trace_file = constants.TEST_DATA + COUNTRY_CODE + "/" + COUNTRY_CODE + '_cbgp_trace_' + SUFFIX + '.txt'
+
+  print 'prefix loading file', loading_file
+  print 'trace_file', trace_file
+  print 'out_cbgp_trace_file', out_cbgp_trace_file
+
+
   cBGP = CBGP("/usr/local/bin/cbgp")
-  fo = open('temp.txt', 'w')
-  cmd = "include /home/anshika/madhur-work/Test_Data/IL/IL_bank_1.cli\n"
-  cBGP.send(cmd)
-  print 'done loading'  
-  fi = open('/home/anshika/madhur-work/Test_Data/IL/IL_bank_traceroute_1.cli')
+  fo = open(out_cbgp_trace_file, 'w')
+
+  cmd_load = 'include ' + loading_file + '\n'
+  print 'load cmd', cmd_load
+  cBGP.send(cmd_load)
+  print 'done loading'
+  
+  fi = open(trace_file)
   for line in fi:
-    print line
     cBGP.send(line)
     ret = cBGP.expect()
-    print ret
     fo.write(ret)
 
-
+  print 'done'
 
   cBGP.finalize()
+
